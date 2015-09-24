45f11895c0e40eedbc61cb1501eb44b6e1e2f4a1
Rabbit.Poi~ <zhuzi@linuser.org>
2015-09-24 09:37:20 +0800

    2015/9/24 alpha 1.0.0.1

--- 萌控二次元/MainWindow.xaml.cs
+++ 萌控二次元/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.Xml;
-using mkacg_talk;
 namespace 萌控二次元
 {
     /// <summary>
@@ -27,97 +16,134 @@ namespace 萌控二次元
         {
             InitializeComponent();
         }
-
-        static bool IsWebResourceAvailable (string webResourceAddress)
+        private void hide (object sender , EventArgs e)
         {
-            try
-            {
-                HttpWebRequest req = (HttpWebRequest)WebRequest.CreateDefault(new Uri(webResourceAddress));
-                req.Method = "HEAD";
-                req.Timeout = 1000;
-                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
-                return (res.StatusCode == HttpStatusCode.OK);
-            }
-            catch (WebException wex)
-            {
-                System.Diagnostics.Trace.Write(wex.Message);
-                return false;
-            }
-        }
+           
+                bg.Visibility = Visibility.Hidden;
+                bg_label.Visibility = Visibility.Hidden;
+                bg_source.Visibility = Visibility.Hidden;
+                bg_text.Visibility = Visibility.Hidden;
 
+
+            /*
+
+
+            
+                bg.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg.Visibility = Visibility.Visible;
+                }));
+                bg_label.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_label.Visibility = Visibility.Visible;
+                }));
+                bg_source.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_source.Visibility = Visibility.Visible;
+                }));
+                bg_text.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_text.Visibility = Visibility.Visible;
+                }));
+
+    */
+        }
         private void MenuItem_Click (object sender , RoutedEventArgs e)
         {
-        
+           
             //开辟线程吧！
             Thread thread = new Thread(new ThreadStart(gethitokoto));
             //启动线程
+            thread.IsBackground = true;
             thread.Start();
-           
-            //  gethitokoto();
+
 
         }
-
         private void gethitokoto ()
         {
-
-            //http://api.hitokoto.us/rand?cat=a&charset=utf-8&encode=xml
             String url = "http://api.hitokoto.us/rand?cat=a&charset=utf-8&encode=xml";
-            bool net = IsWebResourceAvailable(url);
-            if (true == true)
-            {
-
-                XmlElement root = null;
-                XmlDocument xmldoc = new XmlDocument();
-                try
-                {
-                    bg_text.Dispatcher.Invoke(new Action(() =>
-                    {
-                        bg_text.Text = "";
-                    }));
-                
-                    xmldoc.Load("rand.xml");
-                    root = xmldoc.DocumentElement;
-                    XmlNodeList listNodes = null, listsource = null;
-                    listNodes = root.SelectNodes("/result/hitokoto");
-                    listsource = root.SelectNodes("/result/source");
-                    // /result/author 谁说的话
-                    // /result/source 来自哪部
-                    foreach (XmlNode node in listNodes)
-                    {
-                        bg_text.Dispatcher.Invoke(new Action(() =>
-                        {
-                            bg_text.Text += node.InnerText + "\n";
-                        }));
-                    }
-                    foreach (XmlNode node in listsource)
-                    {
-                        bg_source.Dispatcher.Invoke(new Action(() =>
-                        {
-                            bg_source.Content = "来自：" + node.InnerText + "\n";
-                        }));
-                    }
-                    bg_source.Dispatcher.Invoke(new Action(() =>
-                    {
-                        bg_source.Content = "hhh";
-                    }));
-                }
-
-                catch
-                {
-                    bg_text.Dispatcher.Invoke(new Action(() =>
-                    {
-                        bg_text.Text = "出错啦";
-                    }));
-                }
-            }
-            else
+            XmlElement root = null;
+            XmlDocument xmldoc = new XmlDocument();
+            try
             {
                 bg_text.Dispatcher.Invoke(new Action(() =>
                 {
-                    bg_text.Text= "网络连接失败";
+                    bg_text.Text = "";
+                }));
+                Thread.Sleep(200);
+                bg.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg.Visibility = Visibility.Visible;
+                }));
+                bg_label.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_label.Visibility = Visibility.Visible;
+                }));
+                bg_source.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_source.Visibility = Visibility.Visible;
+                }));
+                bg_text.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_text.Visibility = Visibility.Visible;
+                }));
+
+                xmldoc.Load(url);
+                root = xmldoc.DocumentElement;
+                XmlNodeList listNodes = null, listsource = null;
+                listNodes = root.SelectNodes("/result/hitokoto");
+                listsource = root.SelectNodes("/result/source");
+                // /result/author 谁说的话
+                // /result/source 来自哪部
+                foreach (XmlNode node in listNodes)
+                {
+                    bg_text.Dispatcher.Invoke(new Action(() =>
+                    {
+                        bg_text.Text += node.InnerText + "\n";
+                    }));
+
+                }
+                foreach (XmlNode node in listsource)
+                {
+                    bg_source.Dispatcher.Invoke(new Action(() =>
+                    {
+                        bg_source.Content = "来自：" + node.InnerText + "\n";
+                    }));
+                    Thread.Sleep(2000);
+                    /*   DispatcherTimer timer = new DispatcherTimer();
+                       timer.Interval = TimeSpan.FromMilliseconds(1500);
+                       timer.Tick += new EventHandler(hide);  //你的事件
+                       timer.Start();   */
+
+                    bg.Dispatcher.Invoke(new Action(() =>
+                    {
+                        bg.Visibility = Visibility.Hidden;
+                    }));
+                    bg_label.Dispatcher.Invoke(new Action(() =>
+                    {
+                        bg_label.Visibility = Visibility.Hidden;
+                    }));
+                    bg_source.Dispatcher.Invoke(new Action(() =>
+                    {
+                        bg_source.Visibility = Visibility.Hidden;
+                    }));
+                    bg_text.Dispatcher.Invoke(new Action(() =>
+                    {
+                        bg_text.Visibility = Visibility.Hidden;
+
+                    }));
+                    Thread.Sleep(300);
+                }
+            }
+            catch
+            {
+                bg_text.Dispatcher.Invoke(new Action(() =>
+                {
+                    bg_text.Text = "似乎网络出错啦";
                 }));
             }
         }
+ 
         private void MenuItem_Click_1 (object sender , RoutedEventArgs e)
         {
             mkacg_talk.MainWindow mw = new mkacg_talk.MainWindow();
@@ -133,6 +159,11 @@ namespace 萌控二次元
         private void Window_Loaded (object sender , RoutedEventArgs e)
         {
             this.Topmost = true;
+         /*   DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1500);
+            timer.Tick += new EventHandler(hide);  //你的事件
+            timer.Start();
+            */
         }
 
         private void image_MouseLeftButtonDown (object sender , MouseButtonEventArgs e)
@@ -142,5 +173,6 @@ namespace 萌控二次元
                 this.DragMove();
             }
         }
+
     }
 }
