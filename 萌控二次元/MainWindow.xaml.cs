d11f5746cf3454042730f3ebbd73559c522b4c87
kirigaya <zhuzi@linuser.org>
2015-10-11 22:19:23 +0800

    完善了功能代码

--- 萌控二次元/MainWindow.xaml.cs
+++ 萌控二次元/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using System.Xml;
 namespace 萌控二次元
@@ -16,13 +17,27 @@ namespace 萌控二次元
         {
             InitializeComponent();
         }
+        static int themes = 0, bg_showorhide = 0;
+        DispatcherTimer timer = new DispatcherTimer();
+        DispatcherTimer timer_noew = new DispatcherTimer();
         private void MenuItem_Click (object sender , RoutedEventArgs e)
         {
+            //显示一句话
+            timer.Stop();
+            bg.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Visible;
             mkacg_showhitokoto.Class1 mkacgclass = new mkacg_showhitokoto.Class1();
             String[] list = mkacgclass.hitokoto();
             bg_text.Text = "";
             bg_text.Text = list[0] + "\n";
             bg_source.Content = list[1] + "\n";
+            timer.Start();
+        }
+       
+        private void showorhide (object sender , EventArgs e)
+        {
+            //bg.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Visible;
+            bg.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Hidden;
+            timer.Stop();
         }
         private void MenuItem_Click_1 (object sender , RoutedEventArgs e)
         {
@@ -34,10 +49,18 @@ namespace 萌控二次元
         {
             this.Close();
         }
-
+        
         private void Window_Loaded (object sender , RoutedEventArgs e)
         {
             this.Topmost = true;
+            if (themes==0)
+            {
+                image.Source = new BitmapImage(new Uri("Images/Main.png" , UriKind.Relative));
+                themes = 1;
+            }
+            timer.Interval = new TimeSpan(0 , 0 , 4);
+            timer.Tick += new EventHandler(showorhide);
+           
         }
 
         private void image_MouseLeftButtonDown (object sender , MouseButtonEventArgs e)
@@ -47,6 +70,20 @@ namespace 萌控二次元
                 this.DragMove();
             }
         }
-
+        
+        private void MenuItem_Click_3 (object sender , RoutedEventArgs e)
+        {
+            if (themes == 1)
+            {
+                image.Source = new BitmapImage(new Uri("Images/Main1.png" , UriKind.Relative));
+                themes = 0;
+            }
+           else if (themes == 0)
+            {
+                image.Source = new BitmapImage(new Uri("Images/Main.png" , UriKind.Relative));
+                themes = 1;
+            }
+           
+        }
     }
 }
