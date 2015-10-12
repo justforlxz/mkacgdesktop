1625615d81c7d9947c8f4bf27f74fa2be30e7a69
kirigaya <zhuzi@linuser.org>
2015-10-12 09:38:15 +0800

    完善了talk功能

---
+++ talk/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace talk
+{
+    public class Class1
+    {
+       public String main (String text)
+        {
+            String gettext = null;
+            int i = 0;
+            if (InternetGetConnectedState(out i , 0))
+            {
+               
+                if (text != "")
+                {
+                    gettext = ConnectTuLing(text);
+
+                }
+                else
+                {
+                    gettext = "不允许输入空值";
+                }
+            }
+            else
+            {
+                gettext = "网络连接失败";
+            }
+            return gettext;
+
+        }
+        [DllImport("wininet")]
+        private extern static bool InternetGetConnectedState (out int connectionDescription , int reservedValue);
+
+        HttpWebResponse Response = null;
+        public string ConnectTuLing (string p_strMessage)
+        {
+            string result = null;
+            try
+            {
+                String APIKEY = "c32ccaa805b6441be76bc18074f12e51";
+                String _strMessage = p_strMessage;
+                String INFO = Encoding.UTF8.GetString(Encoding.UTF8.GetBytes(_strMessage));
+                String getURL = "http://www.tuling123.com/openapi/api?key=" + APIKEY + "&info=" + INFO;
+                HttpWebRequest MyRequest = (HttpWebRequest)HttpWebRequest.Create(getURL);
+                HttpWebResponse MyResponse = (HttpWebResponse)MyRequest.GetResponse();
+                Response = MyResponse;
+                using (Stream MyStream = MyResponse.GetResponseStream())
+                {
+                    long ProgMaximum = MyResponse.ContentLength;
+                    long totalDownloadedByte = 0;
+                    byte[] by = new byte[1024];
+                    int osize = MyStream.Read(by , 0 , by.Length);
+                    Encoding encoding = Encoding.UTF8;
+                    while (osize > 0)
+                    {
+                        totalDownloadedByte = osize + totalDownloadedByte;
+                        result += encoding.GetString(by , 0 , osize);
+                        long ProgValue = totalDownloadedByte;
+                        osize = MyStream.Read(by , 0 , by.Length);
+                    }
+                }
+                //解析json
+                JsonReader reader = new JsonTextReader(new StringReader(result));
+                while (reader.Read())
+                {
+                    //text中的内容才是你需要的
+                    if (reader.Path == "text")
+                    {
+                        //结果赋值
+                        result = reader.Value.ToString();
+                    }
+                    Console.WriteLine(reader.TokenType + "\t\t" + reader.ValueType + "\t\t" + reader.Value);
+                }
+            }
+            catch (Exception)
+            {
+                result = "网络连接失败";
+            }
+            return result;
+        }
+
+    }
+}
--- 萌控二次元/MainWindow.xaml.cs
+++ 萌控二次元/MainWindow.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Net;
-using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
-using System.Xml;
 namespace 萌控二次元
 {
     /// <summary>
@@ -19,11 +16,12 @@ namespace 萌控二次元
         }
         static int themes = 0;
         DispatcherTimer timer = new DispatcherTimer();
+        DispatcherTimer timerToSendMessages = new DispatcherTimer();
         private void MenuItem_Click (object sender , RoutedEventArgs e)
         {
             //显示一句话
             timer.Stop();
-            bg.Visibility= bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Visible;
+            showorhidetrue();
             mkacg_showhitokoto.Class1 mkacgclass = new mkacg_showhitokoto.Class1();
             String[] list = mkacgclass.hitokoto();
             bg_text.Text = "";
@@ -31,35 +29,37 @@ namespace 萌控二次元
             bg_source.Content = list[1] + "\n";
             timer.Start();
         }
+        private void showorhidetrue ()
+        {
+            bg_white.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Visible;
+            sendbox.Visibility = Visibility.Visible;
+        }
         private void showorhide (object sender , EventArgs e)
         {
-            //bg.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Visible;
-            bg.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Hidden;
+            bg_white.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Hidden;
+            sendbox.Visibility = Visibility.Hidden;
             timer.Stop();
+            timerToSendMessages.Stop();
         }
-        private void MenuItem_Click_1 (object sender , RoutedEventArgs e)
-        {
-            mkacg_talk.MainWindow mw = new mkacg_talk.MainWindow();
-            mw.Show();
-        }
-
         private void MenuItem_Click_2 (object sender , RoutedEventArgs e)
         {
             this.Close();
         }
-        
         private void Window_Loaded (object sender , RoutedEventArgs e)
         {
             this.Topmost = true;
-            if (themes==0)
+            if (themes == 0)
             {
                 image.Source = new BitmapImage(new Uri("Images/Main.png" , UriKind.Relative));
                 themes = 1;
             }
             timer.Interval = new TimeSpan(0 , 0 , 4);
             timer.Tick += new EventHandler(showorhide);
+            timer.Start();
+            timerToSendMessages.Interval = new TimeSpan(0,0,10);
+            timerToSendMessages.Tick += new EventHandler(showorhide);
+            timerToSendMessages.Start();
         }
-
         private void image_MouseLeftButtonDown (object sender , MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -67,7 +67,6 @@ namespace 萌控二次元
                 this.DragMove();
             }
         }
-        
         private void MenuItem_Click_3 (object sender , RoutedEventArgs e)
         {
             if (themes == 1)
@@ -75,12 +74,20 @@ namespace 萌控二次元
                 image.Source = new BitmapImage(new Uri("Images/Main1.png" , UriKind.Relative));
                 themes = 0;
             }
-           else if (themes == 0)
+            else if (themes == 0)
             {
                 image.Source = new BitmapImage(new Uri("Images/Main.png" , UriKind.Relative));
                 themes = 1;
             }
-           
+        }
+        talk.Class1 _talk = new talk.Class1();
+        private void button_Click (object sender , RoutedEventArgs e)
+        {
+            timer.Stop();
+            timerToSendMessages.Stop();
+            showorhidetrue();
+            bg_label.Content = _talk.main(sendbox.Text);
+            timerToSendMessages.Start();
         }
     }
 }
