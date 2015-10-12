2228b83ae8a9baf7ccd940d31fb22686d0e1370b
kirigaya <zhuzi@linuser.org>
2015-10-12 10:44:17 +0800

    重写talk

--- mkacg_showhitokoto/Properties/AssemblyInfo.cs
+++ mkacg_showhitokoto/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("mkacg_showhitokoto")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("萌控二次元动漫台词获取")]
+[assembly: AssemblyDescription("萌控二次元动漫台词获取")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mkacg_showhitokoto")]
+[assembly: AssemblyCompany("萌控二次元")]
+[assembly: AssemblyProduct("萌控二次元")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
--- 萌控二次元/MainWindow.xaml.cs
+++ 萌控二次元/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace 萌控二次元
             //显示一句话
             timer.Stop();
             showorhidetrue();
+            sendbox.Visibility = Visibility.Hidden;
             mkacg_showhitokoto.Class1 mkacgclass = new mkacg_showhitokoto.Class1();
             String[] list = mkacgclass.hitokoto();
             bg_text.Text = "";
@@ -29,12 +30,12 @@ namespace 萌控二次元
             bg_source.Content = list[1] + "\n";
             timer.Start();
         }
-        private void showorhidetrue ()
+        public void showorhidetrue ()
         {
             bg_white.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Visible;
             sendbox.Visibility = Visibility.Visible;
         }
-        private void showorhide (object sender , EventArgs e)
+        public void showorhide (object sender , EventArgs e)
         {
             bg_white.Visibility = bg_label.Visibility = bg_source.Visibility = bg_text.Visibility = Visibility.Hidden;
             sendbox.Visibility = Visibility.Hidden;
@@ -53,6 +54,7 @@ namespace 萌控二次元
                 image.Source = new BitmapImage(new Uri("Images/Main.png" , UriKind.Relative));
                 themes = 1;
             }
+            sendbox.Visibility = Visibility.Hidden;
             timer.Interval = new TimeSpan(0 , 0 , 4);
             timer.Tick += new EventHandler(showorhide);
             timer.Start();
@@ -81,13 +83,44 @@ namespace 萌控二次元
             }
         }
         talk.Class1 _talk = new talk.Class1();
-        private void button_Click (object sender , RoutedEventArgs e)
+
+        private void sendbox_KeyDown (object sender , KeyEventArgs e)
         {
-            timer.Stop();
-            timerToSendMessages.Stop();
-            showorhidetrue();
-            bg_label.Content = _talk.main(sendbox.Text);
-            timerToSendMessages.Start();
+            if (e.Key == Key.Enter)
+            {
+
+                timer.Stop();
+                timerToSendMessages.Stop();
+                showorhidetrue();
+                bg_text.Text = _talk.main(sendbox.Text);
+                sendbox.Text = "";
+                timerToSendMessages.Start();
+            }
+
+        }
+        int count = 0;
+        private void image_MouseDown (object sender , MouseButtonEventArgs e)
+        {
+            count += 1;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0 , 0 , 0 , 0 , 300);
+            timer.Tick += (s , e1) => { timer.IsEnabled = false; count = 0; };
+            timer.IsEnabled = true;
+            if (count % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                /*
+                count = 0;
+
+                this.WindowState = this.WindowState == WindowState.Maximized ?
+
+                              WindowState.Normal : WindowState.Maximized;
+                              */
+                count = 0;
+                timerToSendMessages.Stop();
+                showorhidetrue();
+                timerToSendMessages.Start();
+            }
         }
     }
 }
