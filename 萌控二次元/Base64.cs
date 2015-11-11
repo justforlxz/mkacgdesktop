5d7db5895917567f4ff10e75ef826593d945cb8f
桐ヶ谷 の幻想郷 <zhuzi@linuser.org>
2015-11-11 14:36:35 +0800

    无法解决的3301，睡觉

---
+++ 萌控二次元/Base64.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 萌控二次元
+{
+    class Base64
+    {
+        public int Length;
+        public string mp3Convert2Binary (byte[] bytes)
+        {
+            string result = Convert.ToBase64String(bytes);
+
+
+            return result;
+        }
+
+        public byte[] loadWavSound (string filePath)
+        {
+            FileStream file = new FileStream(filePath , FileMode.Open);
+            BinaryReader binaryReader = new BinaryReader(file);
+            Length = Convert.ToInt32(binaryReader.BaseStream.Length);
+            byte[] bytes = binaryReader.ReadBytes(Length);
+            return bytes;
+        }
+    }
+}
--- 萌控二次元/MainWindow.xaml.cs
+++ 萌控二次元/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Runtime.InteropServices;
 using System.Reflection;
 using System.Collections.Generic;
 using System.Threading;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace 萌控二次元
 {
@@ -20,11 +23,11 @@ namespace 萌控二次元
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public MainWindow ()
         {
-         InitializeComponent();
-         
+            InitializeComponent();
+
         }
 
         /// <summary>
@@ -69,10 +72,10 @@ namespace 萌控二次元
         private void Window_Loaded (object sender , RoutedEventArgs e)
         {
 
-        
-                //开机启动
-                //配置文件
-                if (File.Exists(@"config.xml"))
+
+            //开机启动
+            //配置文件
+            if (File.Exists(@"config.xml"))
             {
                 //read
             }
@@ -98,7 +101,7 @@ namespace 萌控二次元
             timerToSendMessages.Interval = new TimeSpan(0 , 0 , 10);
             timerToSendMessages.Tick += new EventHandler(showorhide);
             timerToSendMessages.Start();
-            someTime_timer.Interval = new TimeSpan(0,0, someTime_random.Next(1,1800));  //随机事件进行消息提醒
+            someTime_timer.Interval = new TimeSpan(0 , 0 , someTime_random.Next(1 , 1800));  //随机事件进行消息提醒
             someTime_timer.Tick += new EventHandler(someTime);
             someTime_timer.Start();
             hp.Visibility = Visibility.Visible;
@@ -127,15 +130,15 @@ namespace 萌控二次元
          */
             double workHeight = SystemParameters.WorkArea.Height;
             double workWidth = SystemParameters.WorkArea.Width;
-           double top = (workHeight - this.Height) ;
-           double left = (workWidth - this.Width) ;
-            element1.SetAttribute("top",top.ToString());
-            element1.SetAttribute("left",left.ToString());
+            double top = (workHeight - this.Height);
+            double left = (workWidth - this.Width);
+            element1.SetAttribute("top" , top.ToString());
+            element1.SetAttribute("left" , left.ToString());
             root.AppendChild(element1);
             doc.AppendChild(root);
             doc.Save("config.xml");
         }
-   
+
         private void image_MouseLeftButtonDown (object sender , MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -197,17 +200,17 @@ namespace 萌控二次元
                         timerToSendMessages.Start();
                         break;
                 }
-                
-              
+
+
             }
         }
-        private void open_autorun()
+        private void open_autorun ()
         {
             RegistryKey HKCU = Registry.CurrentUser;
-            RegistryKey Run = HKCU.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            RegistryKey Run = HKCU.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run" , true);
             try
             {
-                Run.SetValue("MKACG", AppDomain.CurrentDomain.BaseDirectory + "萌控二次元.exe");
+                Run.SetValue("MKACG" , AppDomain.CurrentDomain.BaseDirectory + "萌控二次元.exe");
 
             }
             catch
@@ -254,7 +257,7 @@ namespace 萌控二次元
             List<string> list = redio_r.ConnectTuLing();
             list[0] = System.Web.HttpUtility.UrlDecode(list[0] , System.Text.Encoding.UTF8);
             Console.WriteLine(list[0]);
-            bgmusicplayer.Source = new Uri(list[0]); 
+            bgmusicplayer.Source = new Uri(list[0]);
             bg_text.Text = "";
             bg_source.Content = "";
             timer.Stop();
@@ -276,7 +279,7 @@ namespace 萌控二次元
             {
                 try
                 {
-                    play();  
+                    play();
                 }
                 catch (Exception)
                 {
@@ -339,10 +342,10 @@ namespace 萌控二次元
             }
 
         }
-        
+
         private void play_name_Click (object sender , RoutedEventArgs e)
         {
-           
+
             bg_text.Text = "";
             bg_source.Content = "";
             timer.Stop();
@@ -350,7 +353,7 @@ namespace 萌控二次元
             sendbox.Visibility = Visibility.Hidden;
             timer.Start();
             bgmusicplayer.Play();
-            bg_text.Text = "正在播放:" + play_name_get ;
+            bg_text.Text = "正在播放:" + play_name_get;
             Console.WriteLine(play_name_get);
             play_next.Visibility = Visibility.Visible;
             play_name.Visibility = Visibility.Visible;
@@ -560,7 +563,7 @@ namespace 萌控二次元
             }
         }
 
-    
+
         private void someTime (object sender , EventArgs e)
         {
             someTime_timer.Stop();
@@ -583,7 +586,7 @@ namespace 萌控二次元
 
         private void MenuItem_Click_1 (object sender , RoutedEventArgs e)
         {
-            if (hp_bar_show==0)
+            if (hp_bar_show == 0)
             {
                 hp.Visibility = Visibility.Visible;
                 hp_bar_show = 1;
@@ -592,6 +595,160 @@ namespace 萌控二次元
             {
                 hp.Visibility = Visibility.Hidden;
                 hp_bar_show = 0;
+            }
+        }
+
+        //语音处理
+        [DllImport("winmm.dll" , EntryPoint = "mciSendString" , CharSet = CharSet.Auto)]
+        public static extern int mciSendString (
+         string lpstrCommand ,
+         string lpstrReturnString ,
+         int uReturnLength ,
+         int hwndCallback
+        );
+        public void luyin_on ()
+        {
+            mciSendString("set wave bitpersample 8" , "" , 0 , 0);
+
+            mciSendString("set wave samplespersec 20000" , "" , 0 , 0);
+            mciSendString("set wave channels 2" , "" , 0 , 0);
+            mciSendString("set wave format tag pcm" , "" , 0 , 0);
+            mciSendString("open new type WAVEAudio alias movie" , "" , 0 , 0);
+
+            mciSendString("record movie" , "" , 0 , 0);
+        }
+        public void luyin_save ()
+        {
+            mciSendString("stop movie" , "" , 0 , 0);
+            mciSendString("save movie 1.wav" , "" , 0 , 0);
+            mciSendString("close movie" , "" , 0 , 0);
+        }
+
+        private void sound_on_Click (object sender , RoutedEventArgs e)
+        {
+           luyin_on();
+        
+        }
+
+        private void sound_save_Click (object sender , RoutedEventArgs e)
+        {
+
+            // StreamReader sr = File.OpenText(openFile.FileName);
+            // luyin_save();
+
+            httpRequest hR = new httpRequest();
+            string token_Access = hR.getStrAccess(hR.API_key , hR.API_secret_key);
+            string token_Text = hR.getStrText(hR.API_id , token_Access , "zh" , "1.wav" , "pcm" , "8000");
+            MessageBox.Show(token_Text);
+            
+        }
+
+     
+    }
+    class httpRequest
+    {
+        public string API_id = "7094596";
+        public string API_key = "pxxHWz9KmNsleFBsuWHQ77Hd";
+        public string API_secret_key = "b5a9a0eb50d410b0dccd3eec5fc30388";
+        public string strJSON = "";
+
+        public string getStrAccess (string para_API_key , string para_API_secret_key)
+        {
+            string access_html = null;
+            string access_token = null;
+            //需要到规定的URL获取access token
+
+             string getAccessUrl = "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=" + API_key+ "&client_secret=" +API_secret_key;
+           // String getAccessUrl = "http://vop.baidu.com/server_api?lan=zh&cuid="+API_id+"";
+            try
+            {
+                HttpWebRequest getAccessRequest = WebRequest.Create(getAccessUrl) as HttpWebRequest;
+                getAccessRequest.ContentType = "multipart/form-data";
+                getAccessRequest.Accept = "*/*";
+                getAccessRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)";
+                getAccessRequest.Timeout = 30000;
+                getAccessRequest.Method = "post";
+
+                HttpWebResponse response = getAccessRequest.GetResponse() as HttpWebResponse;
+                using (StreamReader strHttpComback = new StreamReader(response.GetResponseStream() , Encoding.UTF8))
+                {
+                    access_html = strHttpComback.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            JObject jo = JObject.Parse(access_html);
+            access_token = jo["access_token"].ToString();//得到返回的toke
+            return access_token;
+        }
+        public string getStrText (string para_API_id , string para_API_access_token , string para_API_language , string para_API_record , string para_format , string para_Hz)
+        {
+            //方法参数说明:
+            //para_API_id:API_id(你的ID)
+            //para_API_access_token(getStrAccess(...)方法得到的access_token口令)
+            //para_API_language(你要识别的语言, zh, en, ct)
+            //para_API_record(语音文件的路径)
+            //para_format(语音文件的格式)
+            //para_Hz(语音文件的采样率 16000或者8000)
+
+            //该方法返回值:
+            //该方法执行正确返回值是语音翻译的文本,错误是错误号,可以去看百度语音文档,查看对应错误
+
+            string strText = null;
+            string error = null;
+            FileInfo fi = new FileInfo(para_API_record);
+            FileStream fs = new FileStream(para_API_record , FileMode.Open);
+            byte[] voice = new byte[fs.Length];
+            fs.Read(voice , 0 , voice.Length);
+            fs.Close();
+
+            string getTextUrl = "http://vop.baidu.com/server_api?lan=" + "zh" + "&cuid=" + para_API_id + "&token=" + para_API_access_token;
+            HttpWebRequest getTextRequst = WebRequest.Create(getTextUrl) as HttpWebRequest;
+            Console.WriteLine(para_API_id+"\n"+
+                 para_API_access_token + "\n" +
+               para_API_language + "\n" +
+              para_API_record + "\n" + 
+                para_format+"\n"+
+                para_Hz);
+            /* getTextRequst.Proxy = null;
+             getTextRequst.ServicePoint.Expect100Continue = false;
+             getTextRequst.ServicePoint.UseNagleAlgorithm = false;
+             getTextRequst.ServicePoint.ConnectionLimit = 65500;
+             getTextRequst.AllowWriteStreamBuffering = false;*/
+
+            getTextRequst.ContentType = "audio/wav;rate=8000";
+            getTextRequst.ContentLength = fi.Length;
+            getTextRequst.Method = "post";
+            getTextRequst.Accept = "*/*";
+            getTextRequst.KeepAlive = true;
+            getTextRequst.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)";
+            getTextRequst.Timeout = 30000;//30秒连接不成功就中断 
+            using (Stream writeStream = getTextRequst.GetRequestStream())
+            {
+                writeStream.Write(voice , 0 , voice.Length);
+            }
+
+            HttpWebResponse getTextResponse = getTextRequst.GetResponse() as HttpWebResponse;
+            using (StreamReader strHttpText = new StreamReader(getTextResponse.GetResponseStream() , Encoding.UTF8))
+            {
+                strJSON = strHttpText.ReadToEnd();
+                Console.WriteLine(strJSON);
+            }
+            JObject jsons = JObject.Parse(strJSON);//解析JSON
+            if (jsons["err_msg"].Value<string>() == "success.")
+            {
+                strText = jsons["result"][0].ToString();
+                Console.WriteLine(strText);
+                return strText;
+            }
+            else
+            {
+                error = jsons["err_no"].Value<string>() + jsons["err_msg"].Value<string>();
+                Console.WriteLine(error);
+                return error;
             }
         }
     }
